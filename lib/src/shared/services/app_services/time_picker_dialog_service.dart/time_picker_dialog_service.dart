512d52588a7c7356966f56370f8141daf2b67763
Edoardo <edoardofabriziodeiovanna@hotmail.com>
2024-05-06 20:22:24 -0300

    test on IOS

--- lib/src/shared/services/app_services/time_picker_dialog_service.dart/time_picker_dialog_service.dart
+++ lib/src/shared/services/app_services/time_picker_dialog_service.dart/time_picker_dialog_service.dart
@@ -1,16 +1,38 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TimePickerDialogService {
   static Future<TimeOfDay?> showTimePickerDialog(BuildContext context) async {
-    final selectedTime = await showTimePicker(
-      context: context,
-      initialTime: const TimeOfDay(hour: 0, minute: 0),
-      helpText: 'Selecione Horas e Minutos',
-      hourLabelText: 'Horas',
-      minuteLabelText: 'Minutos',
-      errorInvalidText: 'Burro',
-      confirmText: 'CONFIRMAR',
-    );
+    TimeOfDay? selectedTime;
+
+    if (Platform.isAndroid) {
+      selectedTime = await showTimePicker(
+        context: context,
+        initialTime: const TimeOfDay(hour: 0, minute: 0),
+        helpText: 'Selecione Horas e Minutos',
+        hourLabelText: 'Horas',
+        minuteLabelText: 'Minutos',
+        errorInvalidText: 'Impossível recuperar o tempo',
+        confirmText: 'CONFIRMAR',
+      );
+    } else if (Platform.isIOS) {
+      await showCupertinoModalPopup(
+        context: context,
+        builder: (context) {
+          return CupertinoTimerPicker(
+            mode: CupertinoTimerPickerMode.hm,
+            onTimerDurationChanged: (duration) {
+              selectedTime = TimeOfDay(
+                hour: duration.inHours,
+                minute: duration.inMinutes,
+              );
+            },
+          );
+        },
+      );
+    }
 
     return selectedTime;
   }
