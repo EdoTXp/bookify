80245c8e4b0715b80170ae26585c96e263b0390f
Edoardo <edoardofabriziodeiovanna@hotmail.com>
2024-05-20 20:08:08 -0300

    Refactoring Code for Facebook Auth

--- lib/src/core/services/auth_service/auth_strategy/facebook_auth_strategy.dart
+++ lib/src/core/services/auth_service/auth_strategy/facebook_auth_strategy.dart
@@ -3,28 +3,47 @@ import 'package:bookify/src/core/models/user_model.dart';
 import 'package:bookify/src/core/services/auth_service/auth_strategy/auth_strategy.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'dart:math' as math;
 
 class FacebookAuthStrategy implements AuthStrategy {
+  String _generateNonce([int length = 32]) {
+    const charset =
+        '0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._';
+    final random = math.Random.secure();
+    return List.generate(
+      length,
+      (_) => charset[random.nextInt(
+        charset.length,
+      )],
+    ).join();
+  }
+
   @override
   Future<UserModel> signIn() async {
     try {
-      final loginResult = await FacebookAuth.instance.login();
-
-      final facebookOAuthCredential = FacebookAuthProvider.credential(
-        loginResult.accessToken!.tokenString,
+      final loginResult = await FacebookAuth.instance.login(
+        nonce: _generateNonce(),
       );
 
-      final userCredential = await FirebaseAuth.instance.signInWithCredential(
-        facebookOAuthCredential,
-      );
+      if (loginResult.status == LoginStatus.success) {
+        final facebookOAuthCredential = FacebookAuthProvider.credential(
+          loginResult.accessToken!.tokenString,
+        );
 
-      final userModel = UserModel(
-        name: userCredential.user?.displayName ?? 'Sem nome',
-        photo: userCredential.user?.photoURL,
-        signInType: SignInType.facebook,
-      );
+        final userCredential = await FirebaseAuth.instance.signInWithCredential(
+          facebookOAuthCredential,
+        );
 
-      return userModel;
+        final userModel = UserModel(
+          name: userCredential.user?.displayName ?? 'Sem nome',
+          photo: userCredential.user?.photoURL,
+          signInType: SignInType.facebook,
+        );
+
+        return userModel;
+      }
+
+      throw const AuthException('Erro na autentificação');
     } on FirebaseException catch (e) {
       throw AuthException(e.message ?? 'With no message');
     } on Exception catch (e) {
