97462f999a735411218cf42c1e9ae005b1364161
Edoardo Fabrizio De Iovanna <edoardofabriziodeiovanna@hotmail.com>
2026-05-09 13:17:22 -0300

    Add Privacy Policy and Terms of Service pages for Bookify project
    - Created privacy-policy.html outlining data handling practices and user privacy.
    - Created terms.html detailing terms of service, project nature, and liability limitations.

--- lib/src/features/auth/widgets/terms_information.dart
+++ lib/src/features/auth/widgets/terms_information.dart
@@ -1,8 +1,39 @@
+import 'package:bookify/src/core/services/app_services/launcher_service/launcher_service.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:localization/localization.dart';
 
-class TermsInformation extends StatelessWidget {
+class TermsInformation extends StatefulWidget {
   const TermsInformation({super.key});
+
+  @override
+  State<TermsInformation> createState() => _TermsInformationState();
+}
+
+class _TermsInformationState extends State<TermsInformation> {
+  late TapGestureRecognizer _termsRecognizer;
+  late TapGestureRecognizer _privacyRecognizer;
+
+  @override
+  void initState() {
+    super.initState();
+    _termsRecognizer = TapGestureRecognizer()
+      ..onTap = () async => await LauncherService.openUrl(
+        'https://edotxp.github.io/projects/bookify/terms.html',
+      );
+
+    _privacyRecognizer = TapGestureRecognizer()
+      ..onTap = () async => await LauncherService.openUrl(
+        'https://edotxp.github.io/projects/bookify/privacy-policy.html',
+      );
+  }
+
+  @override
+  void dispose() {
+    _termsRecognizer.dispose();
+    _privacyRecognizer.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,6 +46,7 @@ class TermsInformation extends StatelessWidget {
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
+            recognizer: _termsRecognizer,
           ),
           TextSpan(
             text: 'terms-information3'.i18n(),
@@ -24,6 +56,7 @@ class TermsInformation extends StatelessWidget {
             style: TextStyle(
               fontWeight: FontWeight.bold,
             ),
+            recognizer: _privacyRecognizer,
           ),
           TextSpan(
             text: 'terms-information5'.i18n(),
